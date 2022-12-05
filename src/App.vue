93b95716a4fead12cd7497b04ef1fcb12001b2ef
Emanuele213 <vittorius213@gmail.com>
2022-12-05 18:06:54 +0100

    start header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <HeaderPage :arr-header="arrHeader" />
+    <MainPage />
+    <FooterPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HeaderPage from '@/components/HeaderPage.vue';
+import MainPage from '@/components/MainPage.vue';
+import FooterPage from '@/components/FooterPage.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld,
+    HeaderPage,
+    MainPage,
+    FooterPage,
+  },
+  data() {
+    return {
+      arrHeader: [
+        'Home',
+        'Service',
+        'Why Us',
+        'Case Studies',
+        'About',
+        'Blog',
+        '(555) 802-1234',
+      ],
+    };
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Poppins', sans-serif;
+}
+
+ul {
+  list-style: none;
 }
 </style>
---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,72 @@
+<template>
+  <header class="bg-white">
+    <nav>
+      <ul class="container">
+        <li>
+          <img
+            src="@/assets/img/avada-marketing-logo-2x-200x37.png"
+            alt="logo"
+          >
+        </li>
+        <li
+          v-for="menu in arrHeader"
+          :key="menu"
+          class="mod-menu"
+        >
+          {{ menu }}<font-awesome-icon icon="fa-solid fa-angle-down" />
+        </li>
+        <button class="mod-btn">
+          Free Quote
+        </button>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    arrHeader: Array,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.bg-white {
+  background-color: white;
+  width: 100%;
+}
+
+.container {
+  max-width: 1300px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100px;
+  .mod-menu {
+    display: flex;
+    align-items: center;
+    padding: .5rem 1rem;
+    border-bottom: 2px solid white;
+    &:hover{
+      cursor: pointer;
+      color: #F86011;
+      border-color: #F86011;
+    }
+  }
+  .mod-btn {
+    font-size: 1.2rem;
+    border: 0;
+    padding: 1rem 2rem;
+    border-radius: 1rem;
+    color: #F96C23;
+    background-color: #FFE7DA;
+    cursor: pointer;
+    &:hover{
+      color: white;
+      background-color: #F86011;
+    }
+  }
+}
+</style>

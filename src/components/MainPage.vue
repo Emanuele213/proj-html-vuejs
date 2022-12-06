6bc309f1ba8b6cfd2c1c07bb35b4c9cfa8193daf
Emanuele213 <vittorius213@gmail.com>
2022-12-06 10:10:29 +0100

    first part main completed

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,6 +1,35 @@
 <template>
-  <main>
-    Main
+  <main class="main-container">
+    <div class="container-1">
+      <div>
+        <img
+          src="@/assets/img/main1.png"
+          alt="Brand"
+          class="mod-img"
+        >
+      </div>
+      <div class="container-column">
+        <div class="flex-img">
+          <img
+            src="@/assets/img/business-growth.png"
+            alt="businesss"
+          >
+          <h3>Business Growth</h3>
+        </div>
+        <div class="container-text">
+          <h1> Increase Brand Awareness </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Sed auctor placerat
+            luctus. Nullam sit amet ante sed orci
+            convallis gravida et at massa.
+          </p>
+          <button class="mod-btn">
+            Get a Consultation
+          </button>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -11,5 +40,47 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.main-container {
+  max-width: 1200px;
+  margin: 0 auto;
+  background-color: white;
+  margin-top: 10rem;
+  .container-1 {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .mod-img {
+      width: 580px;
+    }
+  }
+  .container-column {
+    width: 500px;
+    .flex-img {
+    display: flex;
+    align-items: center;
+  }
+  .container-text {
+    h1 {
+      font-size: 3rem;
+    }
+    p {
+      font-size: 1.5rem;
+      margin-bottom: 2rem;
+    }
+    .mod-btn {
+    font-size: 1.2rem;
+    border: 0;
+    padding: 1rem 2rem;
+    border-radius: 1rem;
+    color: #F96C23;
+    background-color: #FFE7DA;
+    cursor: pointer;
+    &:hover{
+      color: white;
+      background-color: var(--orange);
+    }
+  }
+  }
+  }
+}
 </style>

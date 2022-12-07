8a5e62235af3f6d93fd3de00cbacfffd1bb91185
Emanuele213 <vittorius213@gmail.com>
2022-12-07 10:34:47 +0100

    completed part 'award'

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -234,6 +234,87 @@
           >
         </div>
       </div>
+      <div class="second-container">
+        <div class="container-half">
+          <h2>We Build Relationships</h2>
+          <!-- eslint-disable-next-line -->
+          <p>Curabitur ac leo nunc. Vestibulum et mauris vel ante finibus maximus nec ut leo. Integer consectetur.</p>
+          <h2>Proprietary Processes</h2>
+          <!-- eslint-disable-next-line -->
+          <p>Curabitur ac leo nunc. Vestibulum et mauris vel ante finibus maximus nec ut leo. Integer consectetur.</p>
+        </div>
+        <div class="container-half-2">
+          <div class="use-flex">
+            <img
+              src="@/assets/img/cerca.png"
+              alt="cerca"
+            >
+            <div>
+              <h1>Accountability</h1>
+              <p>Curabitur ac leo nurc vestibulum</p>
+            </div>
+          </div>
+          <div class="use-flex">
+            <div>
+              <img
+                src="@/assets/img/piu.png"
+                alt="piu'"
+              >
+            </div>
+            <div>
+              <h1>Transparency</h1>
+              <p>Curabitur ac leo nurc vestibulum</p>
+            </div>
+          </div>
+          <div class="use-flex">
+            <img
+              src="@/assets/img/conessione.png"
+              alt="conessione"
+            >
+            <div>
+              <h1>Investment</h1>
+              <p>Curabitur ac leo nurc vestibulum</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="awards-container">
+        <h2>Awards</h2>
+        <div class="container-awards-in">
+          <div>
+            <img
+              src="@/assets/img/award-a-200x191.png"
+              alt="premio 1"
+            >
+            <h3>Winner Seo Master MAGT</h3>
+            <h3>Smart Start Award 2017</h3>
+          </div>
+          <div>
+            <img
+              src="@/assets/img/award-b-200x191.png"
+              alt="premio 1"
+            >
+            <h3>Top Social Media Agencies</h3>
+            <h3>Next Partner 2018</h3>
+          </div>
+          <div>
+            <img
+              src="@/assets/img/award-c-200x191.png"
+              alt="premio 1"
+            >
+            <h3>10 Fastest Growing Abstract</h3>
+            <h3>Solution Providers 2019</h3>
+          </div>
+          <div>
+            <img
+              src="@/assets/img/award-d-200x191.png"
+              alt="premio 1"
+            >
+            <h3>National Excellence Agencie</h3>
+            <h3>Award Winner 2020</h3>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -428,6 +509,7 @@ export default {
   .mod-btn {
     font-size: 1.2rem;
     border: 0;
+    margin-bottom: 3rem;
     padding: 1rem 2rem;
     border-radius: 1rem;
     color: #F96C23;
@@ -471,7 +553,7 @@ export default {
     margin: 0 auto;
     display: flex;
     align-items: center;
-    gap: 2rem;
+    justify-content: space-between;
     .left-part {
       h1 {
         font-size: 3rem;
@@ -502,6 +584,44 @@ export default {
   .mod-img-team {
     width: 550px;
     margin-top: 5rem;
+    margin-bottom: 5rem;
+  }
+  .second-container {
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    .container-half {
+      width: 40%;
+      h2 {
+        margin: 2rem 0;
+      }
+    }
+      h2:first-child {
+        margin: 0 0 2rem 0 ;
+      }
+    .container-half-2 {
+      width: 40%;
+    }
+    .use-flex {
+    display: flex;
+  }
+  }
+  .awards-container {
+    max-width: 1200px;
+    margin: 0 auto;
+    text-align: center;
+    h2 {
+      margin-top: 5rem;
+      margin-bottom: 2rem;
+    }
+    .container-awards-in{
+      display: flex;
+      justify-content: space-between;
+      h3 {
+        font-weight: normal;
+      }
+    }
   }
 }
 </style>
